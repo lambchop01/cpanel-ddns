c154b5a8bb6a51e50766063926f9174c3e079907
Kevin <62576245+lambchop01@users.noreply.github.com>
2021-10-21 23:38:51 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?
+/*********************************************************************************
+*Config Example
+*
+*Make sure this file is in the same folder as update.php and is called config.php
+*********************************************************************************/
+//cPanel API user, password, server, and port
+$cpUser = 'lamb6797';
+$cpPassword = 'mabkk21';
+$cpServer = 'https://lambspork.ca';
+$cpPort = '2083';
+
+//Authorized ddns users, passwords, and hostnames
+$authUsers=array('lamb6797');
+$authPasswords=array('mabkk21');
+$authHostnames=array('dadshome.lambspork.ca');
