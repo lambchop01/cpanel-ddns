840c6b5fead25fa02ccdf3ee60b63cb1b2e2ff3a
Kevin <62576245+lambchop01@users.noreply.github.com>
2021-10-21 23:45:58 -0400

    Update config.php

--- nic/config.php
+++ nic/config.php
@@ -5,12 +5,12 @@
 *Make sure this file is in the same folder as update.php and is called config.php
 *********************************************************************************/
 //cPanel API user, password, server, and port
-$cpUser = 'lamb6797';
-$cpPassword = 'mabkk21';
-$cpServer = 'https://lambspork.ca';
+$cpUser = 'user';
+$cpPassword = 'pass';
+$cpServer = 'https://example.ca';
 $cpPort = '2083';
 
 //Authorized ddns users, passwords, and hostnames
-$authUsers=array('lamb6797');
-$authPasswords=array('mabkk21');
-$authHostnames=array('dadshome.lambspork.ca');
+$authUsers=array('user');
+$authPasswords=array('pass');
+$authHostnames=array('server.example.ca');
